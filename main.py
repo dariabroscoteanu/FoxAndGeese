be931e62436914068827fa0f85cf5f7c2aa77a57
Broscoteanu Daria <72194114+dariabroscoteanu@users.noreply.github.com>
2022-06-02 19:56:05 +0300

    Add fox player vs AI.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import copy
+
 import pygame
 import sys
 import math
@@ -107,6 +109,8 @@ class Graph:
         32: [31, 29, 28]
     }
 
+    middle_positions = [4, 9, 14, 15, 16, 17, 18, 23, 28]
+
     scale = 100
     translation = 100
     point_radius = 20
@@ -192,11 +196,23 @@ class Board:
         pygame.display.update()
 
 
+def get_next_point(fox, goose):
+    [x_fox, y_fox] = fox
+    [x_goose, y_goose] = goose
+    x_new = 2 * x_goose - x_fox
+    y_new = 2 * y_goose - y_fox
+    return [x_new, y_new]
+
+
 class Game:
+    P_MIN = None
+    P_MAX = None
+
     def __init__(self, board, geese=None, fox=None):
         self.board = board
         if geese is None:
             self.geese = [6, 13, 20, 21, 22, 23, 24, 25, 26, 19, 12, 27, 28, 29, 30, 31, 32]  # default config for geese
+            # self.geese = sorted(self.geese)
         else:
             self.geese = geese
 
@@ -222,19 +238,12 @@ class Game:
                 goose_pixels = get_pixels(goose)
                 fox_pixels = get_pixels(self.fox)
                 coords = [[Graph.translation + Graph.scale * x for x in nod] for nod in Graph.nodes]
-                new_point = self.get_next_point(goose_pixels)
+                new_point = get_next_point(fox_pixels, goose_pixels)
                 if new_point in coords:
                     position = get_node_from_pixels(new_point)
                     if position not in self.geese and position != self.fox:
                         return True, position
         return False, None
-
-    def get_next_point(self, goose):
-        [x_fox, y_fox] = self.fox
-        [x_goose, y_goose] = goose
-        x_new = 2 * x_goose - x_fox
-        y_new = 2 * y_goose - y_fox
-        return [x_new, y_new]
 
     def check_endgame(self):
         if len(self.geese) <= 3:
@@ -250,6 +259,191 @@ class Game:
                 return True, "Geese"
 
         return False, ""
+
+    def generate_moves_fox(self):
+        neighbors = Graph.nodes_neighbors[self.fox]
+        moves = [] # [ (destination, piece_taken)]
+        for neighbor in neighbors:
+            if neighbor in self.geese:
+                value, position = self.check_valid_fox(self.fox, neighbor)
+                if value:
+                    if position != neighbor:
+                        new_config = Game(self.board, copy.deepcopy(self.geese), self.fox)
+                        new_config.geese.remove(neighbor)
+                        new_config.fox = position
+                        moves.append(new_config)
+                    else:
+                        new_config = Game(self.board, copy.deepcopy(self.geese), self.fox)
+                        new_config.fox = position
+                        moves.append(new_config)
+
+        return moves
+
+    def generate_moves_geese(self):
+        moves = []
+        for goose in self.geese:
+            neighbors = Graph.nodes_neighbors[goose]
+            for neighbor in neighbors:
+                if neighbor not in self.geese:
+                    value = self.check_valid_geese(goose, neighbor)
+                    if value:
+                        new_config = Game(self.board, copy.deepcopy(self.geese), self.fox)
+                        # new_config = copy.deepcopy(self)
+                        new_config.geese.remove(goose)
+                        new_config.geese.append(neighbor)
+                       # new_config.geese = sorted(new_config.geese)
+                        if new_config not in moves:
+                            moves.append(new_config)
+        return moves
+
+    def compute_score1(self):
+        neighbors = Graph.nodes_neighbors[self.fox]
+        count = 0
+        for elem in neighbors:
+            if elem in self.geese:
+                count += 1
+
+        if count == len(neighbors):
+            return -1000000
+
+        if len(self.geese) <= 3:
+            return 1000000
+
+        geese_taken = 17 - len(self.geese)
+
+        fox_score = 20 * geese_taken
+
+        geese_score = (-20) * count
+
+        return fox_score + geese_score
+
+    def compute_score2(self):
+        score = self.compute_score1()
+        count = 0
+        neighbors = Graph.nodes_neighbors[self.fox]
+        removable = 0
+        for elem in neighbors:
+            if elem in self.geese:
+                value, destination = self.check_valid_fox(self.fox, elem)
+                if value:
+                    if destination != elem:
+                        removable += 1
+                count += 1
+
+        score += 50 * removable
+        score += (-50) * count
+        return score
+
+    def estimate_score(self, h):
+        inf = 1000000000
+        value, winner = self.check_endgame()
+        if winner == self.__class__.P_MAX:
+            return inf + h
+        elif winner == self.__class__.P_MIN:
+            return - inf - h
+        else:
+            return self.compute_score1()
+
+
+class State:
+    def __init__(self, game, current_player, depth, parent=None, score=0):
+        # board is an instance of game class
+        self.game = game
+        self.current_player = current_player
+        self.depth = depth
+        self.parent = parent
+        self.score = score
+        self.next_moves = []
+        self.next_state = None
+
+    def possible_moves(self):
+        if self.current_player == "fox":
+            l_moves = self.game.generate_moves_fox()
+            l_states = [State(move, "geese", self.depth - 1, parent=self) for move in l_moves]
+
+            return l_states
+        else:
+            l_moves = self.game.generate_moves_geese()
+            if self.game.geese in l_moves:
+                l_moves.remove(self.game.geese)
+            l_states = [State(move, "fox", self.depth - 1, parent=self) for move in l_moves]
+            return l_states
+
+    def __str__(self):
+        sir = str(self.game)
+        return sir
+
+    def __repr__(self):
+        sir = str(self)
+        return sir
+
+
+def min_max(state):
+    value, winner = state.game.check_endgame()
+    if state.depth == 0 or value:
+        state.score = state.game.estimate_score(state.depth)
+        return state
+
+    state.next_moves = state.possible_moves()
+    if len(state.next_moves) == 0:
+        # print(state)
+        return state
+    print(state.next_moves)
+
+    moves_with_scores = [min_max(move) for move in state.next_moves]
+
+    if len(moves_with_scores) == 0:
+        return state
+
+    if state.current_player == Game.P_MAX:
+        state.next_state = max(moves_with_scores, key=lambda x: x.score)
+    else:
+        state.next_state = min(moves_with_scores, key=lambda x: x.score)
+    state.score = state.next_state.score
+    print("aici")
+    return state
+
+
+def alpha_beta(alpha, beta, state):
+    value, winner = state.game.check_endgame()
+    if state.depth == 0 or value:
+        state.score = state.game.estimate_score(state.depth)
+        return state
+
+    if alpha > beta:
+        return state
+
+    state.next_moves = state.possible_moves()
+
+    if state.current_player == Game.P_MAX:
+        current_score = float('-inf')
+
+        for move in state.next_moves:
+            new_state = alpha_beta(alpha, beta, move)
+            if current_score < new_state.score:
+                state.next_state = new_state
+                current_score = new_state.score
+            if alpha < new_state.score:
+                alpha = new_state.score
+                if alpha >= beta:
+                    break
+
+    elif state.current_player == Game.P_MIN:
+        current_score = float('inf')
+
+        for move in state.next_moves:
+            new_state = alpha_beta(alpha, beta, move)
+            if current_score > new_state.score:
+                state.next_state = new_state
+                current_score = new_state.score
+
+            if beta > new_state.score:
+                beta = new_state.score
+                if alpha >= beta:
+                    break
+    state.score = state.next_state.score
+
+    return state
 
 
 class Button:
@@ -416,6 +610,125 @@ def draw_options():
         pygame.display.update()
 
 
+def player_vs_ai(algorithm, difficulty):
+    pygame.init()
+    pygame.display.set_caption("Fox and Geese - Broscoteanu Daria Mihaela")
+    display = pygame.display.set_mode(size=(1000, 850))
+
+    background_color = (88, 199, 89)
+    lines_color = (0, 0, 0)
+    board = Board(display, background_color, lines_color)
+    game = Game(board)
+    game.P_MAX = "fox"
+    game.P_MIN = "geese"
+    current_state = State(game, 'fox', difficulty)
+
+    current_state.game.board.draw_game_board(game.geese, game.fox, True)
+    pygame.display.update()
+
+    coords = [[Graph.translation + Graph.scale * x for x in nod] for nod in Graph.nodes]
+
+    moved = True
+    win = ""
+    while moved:
+        if current_state.current_player == "fox":
+            for ev in pygame.event.get():
+                if ev.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if ev.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    for nod in coords:
+                        if euclidean_distance(pos, nod) <= Graph.point_radius:
+                            index = get_node_from_pixels(nod)
+                            valid, dest = current_state.game.check_valid_fox(current_state.game.fox, index)
+                            if valid:
+                                if index in current_state.game.geese:
+                                    current_state.game.geese.remove(index)
+                                current_state.game.fox = dest
+                                current_state.current_player = "geese"
+                                current_state.game.board.draw_game_board(current_state.game.geese, current_state.game.fox, current_state.current_player)
+                                value, winner = current_state.game.check_endgame()
+                                if value:
+                                    if winner == "Fox":
+                                        win = "fox"
+                                        turn_font = pygame.font.SysFont('Lobster', 100)
+                                        geese_turn_text = turn_font.render('Fox Won!', True, (0, 0, 0))
+                                        geese_turn_rect = geese_turn_text.get_rect()
+                                        geese_turn_rect.center = (500, 425)
+                                        display.blit(geese_turn_text, geese_turn_rect)
+                                        current_state.game.board.draw_game_board(current_state.game.geese, current_state.game.fox, current_state.current_player)
+                                        pygame.display.update()
+                                        moved = False
+                                        break
+                                    else:
+                                        win = "geese"
+                                        turn_font = pygame.font.SysFont('Lobster', 100)
+                                        geese_turn_text = turn_font.render('Goose Won!', True, (0, 0, 0))
+                                        geese_turn_rect = geese_turn_text.get_rect()
+                                        geese_turn_rect.center = (500, 425)
+                                        display.blit(geese_turn_text, geese_turn_rect)
+                                        current_state.game.board.draw_game_board(current_state.game.geese, current_state.game.fox, current_state.current_player)
+                                        pygame.display.update()
+                                        moved = False
+                                        break
+                                else:
+                                    continue
+                                # break
+                            else:
+                                current_state.current_player = "geese"
+                                current_state.game.board.draw_game_board(current_state.game.geese, current_state.game.fox, current_state.current_player)
+                                break
+                        current_state.game.board.draw_game_board(current_state.game.geese, current_state.game.fox, current_state.current_player)
+        else:
+            print("here")
+            start_t = (time.time() * 1000)
+            if algorithm == 'minimax':
+                new_state = min_max(current_state)
+            else:
+                new_state = alpha_beta(-500, 500, current_state)
+            current_state.game = new_state.next_state.game
+            current_state.current_player = "fox"
+            final_t = int(round(time.time() * 1000))
+            print("The pc \"thought\" about " + str(final_t - start_t) + " milliseconds.")
+
+            current_state.game.board.draw_game_board(current_state.game.geese, current_state.game.fox, current_state.current_player)
+
+        if not moved:
+            if win == "fox":
+                turn_font = pygame.font.SysFont('Lobster', 100)
+                geese_turn_text = turn_font.render('Fox Won!', True, (255, 255, 255))
+                geese_turn_rect = geese_turn_text.get_rect()
+                geese_turn_rect.center = (500, 425)
+                start_time = time.time()
+                while time.time() - start_time < 8:
+                    display.fill(background_color)
+                    display.blit(geese_turn_text, geese_turn_rect)
+                    pygame.display.update()
+                    for ev in pygame.event.get():
+                        if ev.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit()
+                pygame.quit()
+                sys.exit()
+                # draw_game_board(display, background_color, lines_color, geese, fox, True)
+            elif win == "geese":
+                turn_font = pygame.font.SysFont('Lobster', 100)
+                geese_turn_text = turn_font.render('Goose Won!', True, (255, 255, 255))
+                geese_turn_rect = geese_turn_text.get_rect()
+                geese_turn_rect.center = (500, 425)
+                start_time = time.time()
+                while time.time() - start_time < 8:
+                    display.fill(background_color)
+                    display.blit(geese_turn_text, geese_turn_rect)
+                    pygame.display.update()
+                    for ev in pygame.event.get():
+                        if ev.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit()
+                pygame.quit()
+                sys.exit()
+
 
 def player_vs_player():
     pygame.init()
@@ -569,6 +882,26 @@ def run_game():
     player1, player2, algorithm, difficulty = draw_options()
     if player1 == 1 and player2 == 1:
         player_vs_player()
+    elif player1 == 1 and player2 == 3:
+        player_vs_ai(algorithm, difficulty)
 
 run_game()
 # player_vs_player()
+# pygame.init()
+# pygame.display.set_caption("Fox and Geese - Broscoteanu Daria Mihaela")
+# display = pygame.display.set_mode(size=(1000, 850))
+#
+# background_color = (88, 199, 89)
+# lines_color = (0, 0, 0)
+# board = Board(display, background_color, lines_color)
+# game = Game(board)
+# game.P_MAX = "fox"
+# game.P_MIN = "geese"
+# board = Board(display, background_color, lines_color)
+# game = Game(board, [6, 14, 22, 23, 24])
+#
+# current_state = State(game, 'fox', 1)
+# print(game.geese)
+# moves = game.generate_moves_geese()
+# for move in moves:
+#     print(move.geese)
